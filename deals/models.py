56de5de437e5e0c07f818e373899750260367f5c
jrocamora <jordi@MacBook-Air-de-Jordi.local>
2015-02-12 23:56:30 +0100

    Error import slugify

--- deals/models.py
+++ deals/models.py
@@ -1,6 +1,7 @@
 from django import forms
 from django.db import models
 from django.forms import ModelForm
+from django.utils.text import slugify
 
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
